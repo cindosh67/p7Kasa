6001e673be515a8e6b2574a56debce4c2145eb23
cindy <r.cindy67@gmail.com>
2022-12-12 20:59:22 +0100

    code source

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import dbGif from "./db.gif";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={dbGif} alt="dragonBall" />
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum a
+        repudiandae dolorem esse eos rerum temporibus illum voluptates, ratione
+        perferendis maiores magni. Molestiae est facere earum similique
+        deleniti, ut sunt culpa soluta laborum praesentium aliquid ea
+        temporibus. Tempore commodi quos soluta velit rem eum ad dolorum
+        molestias illo repellat non a vero necessitatibus consequuntur in,
+        recusandae rerum? Vero, nihil illo! Animi, a inventore. Deleniti rerum
+        maxime saepe cumque, voluptates ea fugiat reprehenderit eveniet et
+        incidunt, iste voluptatum officiis explicabo possimus, quibusdam modi
+        quo quasi doloremque!
+      </h1>
     </div>
   );
 }
